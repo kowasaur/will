3e223ebd187b2008a183968b33fd1efeac2b2070
kowasaur <kowasaur@gmail.com>
2021-01-05 11:01:22 +1000

    send myself errors

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+import { DiscordAPIError } from "discord.js";
 import { Client } from "its-not-commando";
 import { Poll } from "./commands/poll";
 import { Propose } from "./commands/propose";
@@ -14,3 +15,10 @@ const client = new Client({
 client.registry.registerCommands([Setup, Propose, Poll])
 
 client.start()
+
+// Send myself errors
+process.on("unhandledRejection", (e: DiscordAPIError) => 
+  client.users.cache.get(client.owner)?.send(
+    e.name + ": ```CSS\n[" + e.message + "]\n" + e.path + "\n." + e.code + "```"
+  )
+);
